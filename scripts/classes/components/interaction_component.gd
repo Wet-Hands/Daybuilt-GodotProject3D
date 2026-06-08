7de3cd95c0ad85b90c62abfe740e5616f294fafb
Ian Day <ian.day@mailbox.org>
2026-06-08 17:11:18 -0400

    Update interaction_component.gd

--- scripts/classes/components/interaction_component.gd
+++ scripts/classes/components/interaction_component.gd
@@ -1,11 +1,17 @@
 class_name InteractionComponent
 extends Component
 
+@export var is_enabled : bool = true
+@export var raycast : RayCast3D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func trigger_raycast() -> void:
+	if !is_enabled: return
+	
+	if raycast.is_colliding():
+		var collider = raycast.get_collider()
+		if collider:
+			pass
